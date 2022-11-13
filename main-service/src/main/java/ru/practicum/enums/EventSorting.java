1af25b976e4357d97bf445756b366be6e4883366
Ann <vlaskina.anyuta@yandex.ru>
2022-11-13 22:43:24 +0300

    add dockerfile/docker-compose

---
+++ main-service/src/main/java/ru/practicum/enums/EventSorting.java
@@ -0,0 +1,5 @@
+package ru.practicum.enums;
+
+public enum EventSorting {
+    EVENT_DATE, VIEWS
+}
---
+++ main-service/src/main/java/ru/practicum/enums/State.java
@@ -0,0 +1,5 @@
+package ru.practicum.enums;
+
+public enum State {
+    PENDING, PUBLISHED, CANCELED
+}
